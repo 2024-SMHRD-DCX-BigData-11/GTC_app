94c6be8d34a3524227901c1c72f67933af134544
a2561024 <a2561024@naver.com>
2024-08-16 14:59:57 +0900

    smhrdB 네트워크 임시

--- lib/plugins/dimigoin_flutter_plugin/lib/dimigoin_flutter_plugin.dart
+++ lib/plugins/dimigoin_flutter_plugin/lib/dimigoin_flutter_plugin.dart
@@ -23,7 +23,7 @@ part 'models/stream_socket.dart';
 final _dio = Dio();
 final _storage = const FlutterSecureStorage();
 
-String apiUrl = "http://172.30.1.30:8080";
+String apiUrl = "http://172.30.1.41:8080"; // 임시
 const socketApiUrl = "http://oci.dimigo.in:4999";
 const dimigoStudentApiUrl = "https://api.dimigo.hs.kr/v1";
 
