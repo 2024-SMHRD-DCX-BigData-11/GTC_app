5ea4ccc4994245a4d126a79b78a74ce49a732ea3
smhrd <dydals3116@naver.com>
2024-08-23 10:37:40 +0900

    프로필 수정 스타일 변경

--- lib/screens/studentManage/student_manage_dialog.dart
+++ lib/screens/studentManage/student_manage_dialog.dart
@@ -1,4 +1,6 @@
 import 'package:dalgeurak/controllers/user_controller.dart';
+import 'package:dalgeurak/plugins/dalgeurak-widget-package/lib/themes/color_theme.dart';
+import 'package:dalgeurak/plugins/dalgeurak-widget-package/lib/themes/text_theme.dart';
 import 'package:dalgeurak/plugins/dalgeurak-widget-package/lib/widgets/dialog.dart';
 import 'package:dalgeurak/plugins/dimigoin_flutter_plugin/lib/dimigoin_flutter_plugin.dart';
 import 'package:dalgeurak/themes/text_theme.dart';
@@ -6,6 +8,13 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:get/get.dart';
 import 'package:jiffy/jiffy.dart';
+import 'package:dalgeurak_widget_package/widgets/blue_button.dart';
+import 'package:dalgeurak_widget_package/widgets/oneline_textfield.dart';
+import 'package:dalgeurak_widget_package/widgets/reason_textfield.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:get/get.dart';
+
 
 class StudentManageDialog {
 
@@ -17,58 +26,95 @@ class StudentManageDialog {
 
   void showProfileEditDialog(UserController userController) {
     Get.dialog(
-      AlertDialog(
-        title: const Text('프로필 수정'),
-        content: SizedBox(
-          width: 300, // 다이얼로그의 너비를 조절합니다.
-          child: ListView(
-            shrinkWrap: true, // ListView가 자식 위젯의 크기에 맞게 조절되도록 합니다.
+      Dialog(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.all(Radius.circular(16)),
+        ),
+        child: Container(
+          child: Stack(
+            alignment: Alignment.center,
             children: [
-              TextField(
-                decoration: const InputDecoration(labelText: '이름'),
-                controller: _nameController,
-                // onChanged: (value) => userController.user.update((user) {
-                //   user?.name = value;
-                // }),
+              SizedBox(width: Get.width * 0.85, height: Get.height * 0.4),
+              Positioned(
+                top: Get.height * 0.03,
+                left: Get.width * 0.08,
+                child: Text("프로필 수정", style: dialogTitle), // 기존 다이얼로그 타이틀 스타일 사용
               ),
-              TextField(
-                decoration: const InputDecoration(labelText: '학년'),
-                controller: _gradeController,
-                keyboardType: TextInputType.number, // 키보드 타입을 숫자로 설정
-                inputFormatters: [
-                  FilteringTextInputFormatter.digitsOnly, // 숫자만 입력 허용
-                ],
-                // onChanged: (value) => userController.user.update((user) {
-                //   user?.gradeNum = int.tryParse(value) ?? 0;
-                // }),
+              Positioned(
+                top: Get.height * 0.06,
+                child: Container(
+                  width: Get.width * 0.64,
+                  child: Divider(color: dalgeurakGrayOne, thickness: 1.0),
+                ),
               ),
-              TextField(
-                decoration: const InputDecoration(labelText: '반'),
-                controller: _classContorller,
-                keyboardType: TextInputType.number, // 키보드 타입을 숫자로 설정
-                inputFormatters: [
-                  FilteringTextInputFormatter.digitsOnly, // 숫자만 입력 허용
-                ],
-                // onChanged: (value) => userController.user.update((user) {
-                //   user?.classNum = int.tryParse(value) ?? 0;
-                // }),
+              Positioned(
+                top: Get.height * 0.09,
+                left: Get.width * 0.08,
+                right: Get.width * 0.08,
+                child: Column(
+                  children: [
+                    TextField(
+                      decoration: const InputDecoration(labelText: '이름'),
+                      controller: _nameController,
+                    ),
+                    SizedBox(height: 10),
+                    TextField(
+                      decoration: const InputDecoration(labelText: '학년'),
+                      controller: _gradeController,
+                      keyboardType: TextInputType.number,
+                      inputFormatters: [
+                        FilteringTextInputFormatter.digitsOnly,
+                      ],
+                    ),
+                    SizedBox(height: 10),
+                    TextField(
+                      decoration: const InputDecoration(labelText: '반'),
+                      controller: _classContorller,
+                      keyboardType: TextInputType.number,
+                      inputFormatters: [
+                        FilteringTextInputFormatter.digitsOnly,
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+              Positioned(
+                bottom: 0,
+                child: Row(
+                  children: [
+                    GestureDetector(
+                      onTap: () {
+                        userController.updateProfile(_nameController.text, int.parse(_gradeController.text), int.parse(_classContorller.text));
+                        Get.back();
+                      },
+                      child: Container(
+                        width: Get.width * 0.4,
+                        height: 44,
+                        decoration: BoxDecoration(
+                          color: dalgeurakBlueOne,
+                          borderRadius: BorderRadius.only(bottomLeft: Radius.circular(16)),
+                        ),
+                        child: Center(child: Text("저장", style: dialogBtn)),
+                      ),
+                    ),
+                    GestureDetector(
+                      onTap: () => Get.back(),
+                      child: Container(
+                        width: Get.width * 0.4,
+                        height: 44,
+                        decoration: BoxDecoration(
+                          color: dalgeurakGrayThree,
+                          borderRadius: BorderRadius.only(bottomRight: Radius.circular(16)),
+                        ),
+                        child: Center(child: Text("취소", style: dialogBtn)),
+                      ),
+                    ),
+                  ],
+                ),
               ),
             ],
           ),
         ),
-        actions: [
-          TextButton(
-            onPressed: () {
-              userController.updateProfile(_nameController.text, int.parse(_gradeController.text), int.parse(_classContorller.text)); // 프로필 업데이트 호출
-              Get.back();
-            },
-            child: const Text('저장'),
-          ),
-          TextButton(
-            onPressed: () => Get.back(),
-            child: const Text('취소'),
-          ),
-        ],
       ),
     );
   }
